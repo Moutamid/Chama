27a0bf230fa6f91937736ab80eda6a787a8235b1
moutamid <lumianokia188@gmail.com>
2024-05-11 14:01:17 +0500

    commit

---
+++ app/src/main/java/com/moutamid/chama/bottomsheets/WithdrawFunds.java
@@ -0,0 +1,15 @@
+package com.moutamid.chama.bottomsheets;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class WithdrawFunds extends BottomSheetDialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
